0a925d98d569273a5f4bd40df95d112db4b7c3f7
manu-devops98 <manuelcappello7@gmail.com>
2021-11-26 16:33:04 +0100

    add esercizio

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ Dati tre array contenenti:
  - una lista ordinata di 5 immagini,
  - una lista ordinata dei relativi 5 luoghi e
  - una lista di 5 news,
-creare un carosello come nella foto allegata. */
+creare un carosello come nella foto allegata.
+MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo l’immagine grande a sinistra e le thumbnails sulla destra in modo da poter stilare lo slider; avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull’aspetto logico.
+MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente servendoci dell’array fornito e un semplice ciclo for che concatena un template literal. Al termine di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente attraverso JavaScript.
+MILESTONE 3
+Al click dell’utente sulle frecce verso l’alto o verso il basso, l’immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi:
+ - titolo e
+ - testo.
+Allo stesso tempo nelle miniature l’immagine attiva dovrà apparire in evidenza rispetto alle altre. */
 
 const items = [
     'img/01.jpg',
@@ -30,59 +39,111 @@ const text = [
 ]
 
 
-const carouselLeft = document.querySelector('.carousel-left .container-img-left');
-const carouselRight = document.querySelector('.carousel-right');
-
+const imageContainer = document.querySelector('.slider .col-left');
+const thumbContainer = document.querySelector('.slider .col-right');
 
 for (let i = 0; i < items.length; i++) {
-    let images = `<img src="${items[i]}" alt="">`;
-    // console.log(imagesLeft);
-    let dot = `<div class ="container-img-right"><img src="${items[i]}" alt=""></div>`;
+    
+    let classElement = '';
 
-    carouselLeft.innerHTML += images;
-    carouselRight.innerHTML += dot;
-    // console.log(imagesLeft, imagesRight);
+    if (i == 0) {
+        classElement = 'first active';
+    } else if (i == items.length - 1) {
+        classElement = 'last';
+    }
+    
+    const image = `
+        <div class="img-container ${classElement}">
+            <img src="${items[i]}" alt="">
+             <div class="text-container">
+                <h2>${title[i]}</h2>
+                <p>${text[i]}</p>
+        </div>
+    `;
+    const thumb = `
+    <div class="thumb-container ${classElement}">
+        <img src="${items[i]}" alt="">
+    </div>
+    `;
+
+    imageContainer.innerHTML += image;
+    thumbContainer.innerHTML += thumb;
 }
 
+const nextButton = document.querySelector('.slider .col-right .next');
+const prevButton = document.querySelector('.col-right .prev');
 
-const imagesDom = document.querySelectorAll('.carousel-left img');
-const dotDom = document.querySelectorAll('.carousel-right img');
+nextButton.addEventListener('click', 
+function() {
+    const elementActive = document.querySelector('.slider .col-left .img-container.active');
+    const thumbActive = document.querySelector('.slider .col-right .thumb-container.active');
 
-
-imagesDom[0].classList.add('first', 'active');
-imagesDom[imagesDom.length - 1].classList.add('last');
-dotDom[0].classList.add('first', 'active');
-dotDom[dotDom.length - 1].classList.add('last');
-
-const nextButton = document.querySelector('.next');
-const prevButton = document.querySelector('.prev');
-
-nextButton.addEventListener('click', function () {
-
-    let imagesActive = document.querySelector('.container-img-left .active');
-    let dotActive = document.querySelector('.container-img-right .active');
-
-    let classes = imagesActive.classList;
+    const listClasses = elementActive.classList;
+    // console.log(listClasses);
 
     let last = false;
+  for (let i = 0; i < listClasses.length; i++) {
 
-    for (let i = 0; i < classes.length; i++) {
-        if (classes[i] == 'last') {
-            last = true;
-            console.log(classes[i]);
-        }
+      if (listClasses[i] == 'last') {
+          last = true;
+      }   
     }
+
+    let lastThumb = thumbActive.classList.contains('last');
+
+    // console.log(lastThumb);
 
     if (last == false) {
-        imagesActive.classList.remove('active');
-        dotActive.classList.remove('active');
+        elementActive.classList.remove('active');
 
-        const imgNext = imagesActive.nextElementSibling;
-        imgNext.classList.add('active');
-        const dotNext = dotActive.nextElementSibling;
-        dotNext.classList.add('active');
-    } else {
-        nextButton.classList.remove('able');
+        const elementNext = elementActive.nextElementSibling;
+
+        elementNext.classList.add('active');
+    } 
+
+    if (lastThumb == false) {
+        thumbActive.classList.remove('active');
+        const thumbNext = thumbActive.nextElementSibling;
+        thumbNext.classList.add('active');
+
+    } 
+
+});
+
+prevButton.addEventListener('click', 
+function() {
+    const elementActive = document.querySelector('.slider .col-left .img-container.active');
+    const thumbActive = document.querySelector('.slider .col-right .thumb-container.active');
+    
+    const listClasses = elementActive.classList;
+    // console.log(listClasses);
+
+    let first = false;
+  for (let i = 0; i < listClasses.length; i++) {
+
+      if (listClasses[i] == 'first') {
+          first = true;
+      }   
     }
+
+    let lastThumb = thumbActive.classList.contains('first');
+
+    // console.log(lastThumb);
+
+    if (first == false) {
+        elementActive.classList.remove('active');
+
+        const elementNext = elementActive.previousElementSibling;
+
+        elementNext.classList.add('active');
+    } 
+
+    if (lastThumb == false) {
+        thumbActive.classList.remove('active');
+        const thumbNext = thumbActive.previousElementSibling;
+        thumbNext.classList.add('active');
+
+    } 
+
 
 });
